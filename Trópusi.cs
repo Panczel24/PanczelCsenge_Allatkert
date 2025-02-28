10b294bd7b878faf63825b9d6f1c9faff6fcacb7
Panczel24 <pcsenge8@gmail.com>
2025-02-28 20:19:48 +0100

    Trópusi állatok kész

---
+++ Trópusi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllatkertOOp
+{
+    internal class Trópusi
+    {
+        int sebesseg;
+        string nev;
+        string nem;
+        int eletkor;
+
+        public int Sebesseg { get => sebesseg; set => sebesseg = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Nem { get => nem; set => nem = value; }
+        public int Eletkor { get => eletkor; set => eletkor = value; }
+
+        public Trópusi(int sebesseg, string nev, string nem, int eletkor)
+        {
+            Sebesseg = sebesseg;
+            Nev = nev;
+            Nem = nem;
+            Eletkor = eletkor;
+        }
+    }
+}
